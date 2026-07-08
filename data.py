b17d746168b5d14d7aedc8c7172d7a4548360ad0
Edima <edimaudo@gmail.com>
2026-07-08 13:38:46 -0400

    Update data.py

--- data.py
+++ data.py
@@ -4,17 +4,17 @@ import plotly.express as px
 
 # Load Datasets
 path = "/data/"
-mean_sea_surface_temp_anomaly = pd.read_csv = path + "Mean sea surface temperature anomalies.csv"
-crop_yield = pd.read_csv = path + "Crop yield (disaggregated).csv"
-environmental_tax = pd.read_csv = path + "Environmental taxes (disaggregated).csv"
-greenhouse_gas_emissions = pd.read_csv = path + "Greenhouse gas emissions per capita.csv"
-livestock_yield = pd.read_csv = path + "Livestock yield (disaggregated).csv"
-mean_surface_temp_anomaly = pd.read_csv = path + "Mean surface temperature anomalies.csv"
-meterological_monitor = pd.read_csv = path + "Meteorological monitoring network (disaggregated).csv"
-power_generation = pd.read_csv = path + "Power generation (disaggregated).csv"
-rainfall_anomaly = pd.read_csv = path + "Rainfall anomalies.csv"
-sea_level_anomaly = pd.read_csv = path + "Sea level anomalies.csv"
-tourism_arrival = pd.read_csv = path + "Tourist arrivals (disaggregated).csv"
+mean_sea_surface_temp_anomaly = pd.read_csv(path + "Mean sea surface temperature anomalies.csv")
+crop_yield = pd.read_csv(path + "Crop yield (disaggregated).csv")
+environmental_tax = pd.read_csv(path + "Environmental taxes (disaggregated).csv")
+greenhouse_gas_emissions = pd.read_csv(path + "Greenhouse gas emissions per capita.csv")
+livestock_yield = pd.read_csv(path + "Livestock yield (disaggregated).csv")
+mean_surface_temp_anomaly = pd.read_csv(path + "Mean surface temperature anomalies.csv")
+meterological_monitor = pd.read_csv(path + "Meteorological monitoring network (disaggregated).csv")
+power_generation = pd.read_csv(path + "Power generation (disaggregated).csv")
+rainfall_anomaly = pd.read_csv(path + "Rainfall anomalies.csv")
+sea_level_anomaly = pd.read_csv(path + "Sea level anomalies.csv")
+tourism_arrival = pd.read_csv(path + "Tourist arrivals (disaggregated).csv")
 
 # Set up dataset
 
